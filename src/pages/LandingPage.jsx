13227df6be8835a53b571e4d5cf5231f361cec15
Ritojnan <ritojnanm@gmail.com>
2024-03-17 18:18:00 +0530

    videopage added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ChangeRole from "../components/ChangeRole";
 import Preview from "../components/Preview";
+import VideoPage from "../pages/VideoPage";
 export default function LandingPage() {
   return <>
   <ChangeRole/>
 <Preview/>
+<VideoPage/>
   </>;
 }
