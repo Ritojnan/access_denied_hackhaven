bea02a09645d2d79ffea8540380bdbd2d4fb38c3
varun jajoo <varun.jajoo18@gmail.com>
2024-03-17 20:36:28 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { HMSRoomProvider } from "@100mslive/react-sdk";
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
